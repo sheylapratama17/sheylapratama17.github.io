ab926efce5434caa852d5e2b512a83849dfbef63
sheylapratama17 <rifkykerja171103@gmail.com>
2026-03-01 14:18:57 +0700

    Add files via upload

---
+++ javascript/skrip.js
@@ -0,0 +1,13 @@
+console.log("halo")
+
+gambar3=document.getElementById("logo1");
+gambar3.style.margin="1em auto";
+gambar3.style.display="block";
+gambar3.style.marginBottom="4em";
+
+judul=document.querySelector("#konten1 h1");
+judul.innerHTML="selamat datang.";
+
+
+teks1=document.querySelector("#konten1 p");
+teks1.innerHTML="website ini buatan tangan oleh Rifky Pratama Wahyu Ramadhan, selamat datang dan menikmati website baru ini. semoga website ini membawa dukungan positif dan bermanfaat.";
